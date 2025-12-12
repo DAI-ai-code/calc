370b001b0b23e27bcbf3229ea114b6c456882d83
DAI-ai-code <ajcs2508@gmail.com>
2025-12-12 15:12:14 +0530

    multiplication added in calulator file

--- calculator.py
+++ calculator.py
@@ -2,6 +2,10 @@ class Calculator:
     def add(self,a,b):
         return a+b
 
+    def multiply(self, a,b):
+        return a*b
 
 calc = Calculator()
 print(calc.add(1,2))
+print(calc.multiply(1,2))
+
